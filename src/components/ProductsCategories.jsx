60b1bb5b13353e642e89f247ba2bd97a5f7a7560
Jing <jingli0319@gmail.com>
2025-01-16 08:54:18 +0100

    fix image rendering problem

--- src/components/ProductsCategories.jsx
+++ src/components/ProductsCategories.jsx
@@ -1,30 +1,26 @@
 import { NavLink } from "react-router-dom";
 import CategoryCard from "./CategoryCard.jsx";
+import category_women from "../assets/category_women.png";
+import category_men from "../assets/category_men.png";
+import category_others from "../assets/category_others.png";
+import category_jewelry from "../assets/category_jewelry.png";
+import category_electronics from "../assets/category_others.png";
 
 const ProductsCategories = () => {
   return (
     <div className="products-categories">
       <NavLink to="/products/women">
-        <CategoryCard
-          description="Women"
-          cover="/assets/category_women.png"
-        />
+        <CategoryCard description="Women" cover={category_women} />
       </NavLink>
 
       <NavLink to="/products/men">
-        <CategoryCard description="Men" cover="/assets/category_man.png" />
+        <CategoryCard description="Men" cover={category_men} />
       </NavLink>
       <NavLink to="/products/jewelry">
-        <CategoryCard
-          description="Jewelry"
-          cover="/assets/category_jewelry.png"
-        />
+        <CategoryCard description="Jewelry" cover={category_jewelry} />
       </NavLink>
       <NavLink to="/products/electronics">
-        <CategoryCard
-          description="Electronics"
-          cover="/assets/category_electronics.png"
-        />
+        <CategoryCard description="Electronics" cover={category_electronics} />
       </NavLink>
     </div>
   );

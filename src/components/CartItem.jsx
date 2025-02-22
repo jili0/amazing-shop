b8aef4a1b53173274c69f729d7835ad560aee99d
Jing <jingli0319@gmail.com>
2025-02-22 02:02:01 +0100

    remove item from cart when quantity equals 0

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -15,8 +15,8 @@ const CartItem = ({ product, quantity }) => {
   const decreaseQuantity = () => {
     setCart((cart) => {
       return cart.map((item) =>
-        item.productId === id ? { ...item, quantity: item.quantity - 1 } : item
-      );
+        item.productId === id && item.quantity > 0 ? { ...item, quantity: item.quantity - 1 } : item
+      ).filter((item) => item.quantity > 0);
     });
   };
   const handleRemove = () => {
